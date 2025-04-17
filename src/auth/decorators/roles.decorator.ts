c1dcc077a7c02cd517932ec1982dd7a81706f357
RomanCartman <romanas911@icloud.com>
2025-04-17 20:33:23 +0800

    Удалили не нужный гуард по ролям. Теперь у нас есть permission guard который проверяет прямые разрешения для пользователей, а если таковых нет проверяет разрешения в ролях пользователя.

--- src/auth/decorators/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { RoleName } from '../../users/services/role.service';
-
-export const Roles = (...roles: RoleName[]) => SetMetadata('roles', roles); 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { RolesGuard } from '../auth/guards/roles.guard';
-import { Roles } from '../auth/decorators/roles.decorator';
-import { RoleName } from './services/role.service';
 import { PermissionGuard, RequirePermissions } from './guards/permission.guard';
+import { RoleName } from './services/role.service';
 import { PermissionName } from './enums/permission.enum';
 
 @Controller('admin/users')
@@ -13,7 +11,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard, PermissionGuard)
   @RequirePermissions(PermissionName.MANAGE_USERS)
   async findAll(@Query('role') role?: RoleName) {
     if (role) {
